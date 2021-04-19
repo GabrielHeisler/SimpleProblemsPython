a793d02123180d2c6e1e6cc1568945557f973acd
Gabriel Vinícius Heisler <61278049+GabrielHeisler@users.noreply.github.com>
2021-04-19 15:21:36 -0300

    Add files via upload

---
+++ ProblemsOnFourOperations/4.py
@@ -0,0 +1,9 @@
+
+#4. The cost of 21 TV sets is $95844. Find the cost of one TV set.
+
+cost = 95844
+amount = 21
+
+cost_per_tv = cost / amount
+
+print("One TV cost", cost_per_tv, "dolars")
---
+++ ProblemsOnFourOperations/5.py
@@ -0,0 +1,8 @@
+
+#5. A factory produces 24532 bulbs in a month. What is its annual production?
+
+bulbs_per_month = 24532
+
+bulbs_per_year = bulbs_per_month * 12
+
+print("The factory produces", bulbs_per_year, "bulbs per year")
