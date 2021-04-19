f54703e782f87ddda5fdbe8f6501d0eccf7dc1b1
Gabriel Vinícius Heisler <61278049+GabrielHeisler@users.noreply.github.com>
2021-04-19 15:21:15 -0300

    Add files via upload

---
+++ ProblemsOnFourOperations/2.py
@@ -0,0 +1,9 @@
+
+#2. A shopkeeper has 2425 boxes of 24 pencils each. How many pencils do all the boxes have in all?
+
+boxes = 2425
+pencils_per_boxes = 24
+
+total = boxes * pencils_per_boxes
+
+print("The total amount of pencils is", total)
---
+++ ProblemsOnFourOperations/3.py
@@ -0,0 +1,9 @@
+
+#3. Linda bought a coat for $2265 and a saree for $2150. She gave $5000 to the shopkeeper. How much money did the shopkeeper return to her?
+
+coat = 2265
+saree = 2150
+
+change = 5000 - coat - saree
+
+print("The shopkeeper returned", change, "dolars to her")
