4657c67145df1679b83619b8cd204e6f912a9e99
Gabriel Vinícius Heisler <61278049+GabrielHeisler@users.noreply.github.com>
2021-04-19 15:20:47 -0300

    Add files via upload

---
+++ ProblemsOnFourOperations/1.py
@@ -0,0 +1,9 @@
+
+#1. The population of a town is 198568. Out of them 45312 are men and 35678 are women. Find the number of children in the town.
+
+population = 198568
+men = 45312
+women = 35678
+children = population - men - women
+
+print("The amount of children in this city is", children)
