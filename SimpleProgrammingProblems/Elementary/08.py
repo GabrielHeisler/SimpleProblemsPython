2a37907b79f367c36e8de787cf123a43008396f1
Gabriel Heisler <gvheisler@inf.ufsm.br>
2021-04-22 11:23:33 -0300

    08 and 09 added

--- SimpleProgrammingProblems/Elementary/08.py
+++ SimpleProgrammingProblems/Elementary/08.py
@@ -2,3 +2,14 @@
 #08 - Write a program that prints all prime numbers. (Note: if your programming language does not support arbitrary size numbers,
 #     printing all primes up to the largest number you can easily represent is fine too.)
 
+#     In this case I printed all prime numbers in an interval.
+
+first= 10
+last = 100
+for num in range(first, last+1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
